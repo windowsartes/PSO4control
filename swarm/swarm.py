730d0a0c2fd9dc1a871c9c1489d262b9d09b31f9
Nikolai Makarov <makarov.nikolai20@gmail.com>
2023-06-14 18:26:35 +0300

    early stopping option added; now every particle store it's path's len; inertia param updated; verbose option added

--- swarm/swarm.py
+++ swarm/swarm.py
@@ -16,7 +16,7 @@ class Particle:
         field_width: float = self._swarm.scene.field.width
         field_height: float = self._swarm.scene.field.height
 
-        if self.swarm.scene.spawn_type == "full location":
+        if self.swarm.scene.spawn_type == "full_location":
             self._position: np.ndarray = np.array([uniform(0, field_width), uniform(0, field_height)])
             self._velocity: np.ndarray = np.array([uniform(-field_width, field_width),
                                                    uniform(-field_height, field_height)])
@@ -34,7 +34,7 @@ class Particle:
             else:            # bottom
                 self._position: np.ndarray = np.array([uniform(0, field_width), field_height])
                 self._velocity: np.ndarray = np.array([uniform(-field_width, field_width), uniform(-field_height, 0)])
-        elif self.swarm.scene.spawn_type == "small area":
+        elif self.swarm.scene.spawn_type == "small_area":
             factor: float = self.swarm.scene.factor
             start_location: np.ndarray = self.swarm.scene.spawn_start_location
             if self.swarm.scene.edge == 0:    # left
@@ -60,6 +60,12 @@ class Particle:
                                                         (field_width/2, field_height/2))
         self._best_position: np.ndarray = self._position
 
+        self._path_length: float = 0
+
+        self.w = 1
+        self.c1 = 2
+        self.c2 = 2
+
     @property
     def best_score(self):
         return self._best_score
@@ -84,7 +90,15 @@ class Particle:
     def position(self):
         return self._position
 
-    def update(self):
+    @property
+    def path_length(self):
+        return self._path_length
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    def update(self, current_iteration: int):
 
         field_target_function = self.swarm.scene.field.target_function
         field_width = self.swarm.scene.field.width
@@ -93,10 +107,15 @@ class Particle:
         r_personal: float = np.random.uniform()
         r_global: float = np.random.uniform()
 
-        self._velocity = 1 * self._velocity + 2*r_personal*(self._best_position - self._position) + \
-                         2*r_global*(self._swarm.best_global_position - self._position)
+        self._velocity = self.w * self._velocity + self.c1*r_personal*(self._best_position - self._position) + \
+                         self.c2*r_global*(self._swarm.best_global_position - self._position)
+
+        if current_iteration % 50 == 0:
+            self.w /= 2
 
         self._position = self._velocity + self._position
+
+        self._path_length += np.linalg.norm(self._velocity)
 
         current_score = field_target_function(self._position[0], self._position[1],
                                               (field_width/2, field_height/2))
@@ -128,6 +147,10 @@ class Swarm:
                 self._best_global_score = self.particles[i].best_score
                 self._best_global_position = self.particles[i].best_position
 
+        if self.scene.verbose > 0:
+            self.show_current_position("Начальное положение")
+
+    def show_current_position(self, title: str):
         coordinates = []
         for i in range(self._n_particles):
             coordinates.append(list(self.particles[i].position))
@@ -138,7 +161,7 @@ class Swarm:
         axes = plt.gca()
         axes.set_xlim(0, self.scene.field.width)
         axes.set_ylim(0, self.scene.field.height)
-        axes.set_title("Начальное положение")
+        axes.set_title(title)
         plt.show()
 
     @property
@@ -157,7 +180,52 @@ class Swarm:
     def best_global_position(self, new_position: list[float] | np.ndarray):
         self._best_global_position = new_position
 
-    def release_the_swarm(self):
-        for i in range(self._n_iterations):
+    def release_the_swarm(self) -> tuple[int, float, float, int]:
+        early_stopping_small_velocity_count = 0
+        early_stopping_small_velocity = False
+
+        early_stopping_around_answer_count = 0
+        early_stopping_around_answer = False
+
+        eps_position = 0.001
+        eps_velocity = 0.001
+
+        ratio = 0.75
+
+        for i in range(1, self._n_iterations + 1):
             for j in range(len(self.particles)):
-                self.particles[j].update()
+                self.particles[j].update(i)
+            for j in range(len(self.particles)):
+                if np.linalg.norm(self.particles[j].position - self.scene.answer.position) < eps_position:
+                    early_stopping_around_answer_count += 1
+                if np.linalg.norm(self.particles[j].velocity) < eps_velocity:
+                    early_stopping_small_velocity_count += 1
+
+            if early_stopping_around_answer_count > ratio * self._n_particles:
+                early_stopping_around_answer = True
+                break
+            elif early_stopping_small_velocity_count > ratio * self._n_particles:
+                early_stopping_small_velocity = True
+                break
+
+            if i % 100 == 0:
+                if self.scene.verbose > 1:
+                    self.show_current_position(str(i))
+
+            early_stopping_around_answer_count = 0
+            early_stopping_small_velocity_count = 0
+
+        total_path_length: float = 0
+        for j in range(self._n_particles):
+            total_path_length += self.particles[j].path_length
+
+        result = [i, self.best_global_score, total_path_length]
+
+        if early_stopping_around_answer:
+            result = result + [1]
+        elif early_stopping_small_velocity:
+            result = result + [2]
+        else:
+            result = result + [0]
+
+        return tuple(result)
