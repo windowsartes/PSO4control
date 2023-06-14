f8d606b31a9b5eb844d3b0ce6167f6ab9678ebd1
Nikolai Makarov <makarov.nikolai20@gmail.com>
2023-06-14 18:18:30 +0300

    lil custom logger added

---
+++ logger/custom_logger.py
@@ -0,0 +1,11 @@
+import typing as tp
+
+
+class CustomLogger:
+    def __init__(self, path_to_file: str):
+        self._path_to_file = path_to_file
+
+    def write(self, content: list[tp.Any], mode: str):
+        with open(self._path_to_file, mode) as logs:
+            row = ", ".join([str(elem) for elem in content])
+            logs.write(row + "\n")
