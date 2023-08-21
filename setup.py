f8bf79fa10fc32a0a97d7414f2876ec4cf40bfc2
Nikolai Makarov <makarov.nikolai20@gmail.com>
2023-08-22 00:15:39 +0700

    setup files and requirement added

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name='PSO', version='0.1.0', packages=find_packages())
+
+setup(
+    name="pso4control",
+    version="1.0.0",
+    description="Lil package for studying  applicability of PSO in control theory",
+    author="Nikolai Makarov",
+    author_email="makarov.nikolai20@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "click==8.1.7",
+        "numpy==1.24.3",
+        "seaborn==0.12.2",
+        "sympy==1.12",
+        "matplotlib==3.7.1",
+    ],
+    extras_requires={
+        "log_parsing": ["jupyter"]
+    },
+)
