6b582caaa265901c720b8f5d869475f2ba765504
Nikolai Makarov <makarov.nikolai20@gmail.com>
2023-09-07 17:00:59 +0300

    gradient_methods update according to mype and flake8

--- gradient_methods/gradient_methods.py
+++ gradient_methods/gradient_methods.py
@@ -7,75 +7,78 @@ import matplotlib
 from matplotlib import pyplot as plt
 from abc import ABC, abstractmethod
 
+import typing as tp
+
 
 class GradientMethodInterface(ABC):
     @abstractmethod
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
     @property
     @abstractmethod
-    def position(self):
+    def position(self) -> np.ndarray[tp.Any, np.dtype[np.float64]]:
         pass
 
     @position.setter
     @abstractmethod
-    def position(self, new_position: np.ndarray):
+    def position(self, new_position: np.ndarray[tp.Any, np.dtype[np.float64]]) -> None:
         pass
 
     @property
     @abstractmethod
-    def total_path_length(self):
+    def total_path_length(self) -> float:
         pass
 
     @abstractmethod
-    def _correct_position(self):
+    def _correct_position(self) -> None:
         pass
 
     @abstractmethod
-    def run(self):
+    def run(self) -> tuple[tp.Any, ...]:
         pass
 
     @abstractmethod
-    def show_current_position(self, titel: str):
+    def show_current_position(self, title: str) -> None:
         pass
 
 
 class GradientMethodBase(GradientMethodInterface):
-    def __init__(self, n_iterations: int, scene):
+    def __init__(self, n_iterations: int, scene):  # type: ignore
         self._scene = scene
         self._score: float = float("-inf")
         self._n_iterations: int = n_iterations
 
-        self._total_path_length = 0
+        self._total_path_length: float = 0
 
         edge: int = np.random.randint(4)
+        self._position: np.ndarray[tp.Any, np.dtype[np.float64]]
         if edge == 0:  # left
-            self._position: np.ndarray = np.array([0, uniform(0, self._scene.field.height)])
+            self._position = np.array([0, uniform(0, self._scene.field.height)])
         elif edge == 1:  # right
-            self._position: np.ndarray = np.array([self._scene.field.width, uniform(0, self._scene.field.height)])
+            self._position = np.array([self._scene.field.width, uniform(0, self._scene.field.height)])
         elif edge == 2:  # top
-            self._position: np.ndarray = np.array([uniform(0, self._scene.field.width), 0])
+            self._position = np.array([uniform(0, self._scene.field.width), 0])
         else:  # bottom
-            self._position: np.ndarray = np.array([uniform(0, self._scene.field.width), self._scene.field.height])
+            self._position = np.array([uniform(0, self._scene.field.width), self._scene.field.height])
 
         if self._scene.verbosity.value > 1:
             self.show_current_position("Начальное положение")
 
     @property
-    def position(self):
+    def position(self) -> np.ndarray[tp.Any, np.dtype[np.float64]]:
         return self._position
 
     @position.setter
-    def position(self, new_position: np.ndarray):
+    def position(self, new_position: np.ndarray[tp.Any, np.dtype[np.float64]]) -> None:
         self._position = new_position
         self._correct_position()
 
     @property
-    def total_path_length(self):
+    def total_path_length(self) -> float:
         return self._total_path_length
 
-    def _correct_position(self):
+    def _correct_position(self) -> None:
         if self._position[0] < 0:
             self._position[0] = 0
         elif self._position[0] > self._scene.field.width:
@@ -86,7 +89,7 @@ class GradientMethodBase(GradientMethodInterface):
         elif self._position[1] > self._scene.field.height:
             self._position[1] = self._scene.field.height
 
-    def show_current_position(self, title: str):
+    def show_current_position(self, title: str) -> None:
         correctness_scale = 100
 
         figure = pickle.load(open("./stored_field/field.pickle", "rb"))
@@ -119,12 +122,12 @@ class GradientMethodBase(GradientMethodInterface):
 
 
 class GradientLift(GradientMethodBase):
-    def __init__(self, n_iterations: int, scene):
+    def __init__(self, n_iterations: int, scene) -> None:  # type: ignore
         super().__init__(n_iterations, scene)
 
     def run(self) -> tuple[int | float, ...]:
         for i in range(1, self._n_iterations + 1):
-            current_gradient: np.ndarray = self._scene.field.gradient(*self._position)
+            current_gradient: np.ndarray[tp.Any, np.dtype[np.float64]] = self._scene.field.gradient(*self._position)
 
             current_norm = np.linalg.norm(current_gradient)
             if current_norm < self._scene.hyperparameters.early_stopping_epsilon:
@@ -136,7 +139,7 @@ class GradientLift(GradientMethodBase):
 
             self._position = self._position + current_gradient
             self._correct_position()
-            self._total_path_length += np.linalg.norm(current_gradient)
+            self._total_path_length += float(np.linalg.norm(current_gradient))
             self._score = self._scene.field.target_function(*self._position)
 
             if self._scene.verbosity.value > 0:
@@ -150,13 +153,12 @@ class GradientLift(GradientMethodBase):
 
 
 class NewtonMethod(GradientMethodBase):
-    def __init__(self, n_iterations: int, scene):
+    def __init__(self, n_iterations: int, scene):  # type: ignore
         super().__init__(n_iterations, scene)
 
     def run(self) -> tuple[int | float, ...]:
-        eps_gradient: float = 0.0001
         for i in range(1, self._n_iterations + 1):
-            current_gradient: np.ndarray = self._scene.field.gradient(*self._position)
+            current_gradient: np.ndarray[tp.Any, np.dtype[np.float64]] = self._scene.field.gradient(*self._position)
             hessian_inv = np.linalg.inv(self._scene.field.hessian(*self._position))
 
             current_shift = hessian_inv @ current_gradient
@@ -172,11 +174,11 @@ class NewtonMethod(GradientMethodBase):
             self._position = self._position - current_shift
             self._correct_position()
 
-            self._total_path_length += np.linalg.norm(current_gradient)
+            self._total_path_length += float(np.linalg.norm(current_gradient))
             self._score = self._scene.field.target_function(*self._position)
 
             if self._scene.verbosity.value > 0:
-                if i % self._score.verbosity.show_period == 0:
+                if i % self._scene.verbosity.show_period == 0:
                     self.show_current_position(str(i))
 
         return (i,
