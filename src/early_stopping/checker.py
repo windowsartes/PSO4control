9ca1768bd4d2a437e4a83746c6f7d67c2d72be49
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-07-18 00:06:24 +0300

    added proper type for the list of particles

--- src/early_stopping/checker.py
+++ src/early_stopping/checker.py
@@ -5,6 +5,7 @@ import numpy as np
 from pydantic import BaseModel
 
 from src.early_stopping import stopping_params
+from src.solvers.swarm.particle import Particle
 
 
 class EarlyStopCheckerInterface(ABC):
@@ -22,23 +23,25 @@ def checker(
     return cls
 
 
-# TO-DO тайпинг на ParticleInterface
 @checker
 class SwarmEarlyStopChecker(EarlyStopCheckerInterface):
-    def __init__(self, params: stopping_params.SwarmStoppingParams):
+    def __init__(
+        self,
+        params: stopping_params.SwarmStoppingParams,
+    ):
         super().__init__()
 
         self._params: stopping_params.SwarmStoppingParams = params
 
     def check(
         self,
-        particles: "list[Particle]",
+        particles: list[Particle],
     ) -> bool:
         return self.check_position(particles) or self.check_velocity(particles)
 
     def check_position(
         self,
-        particles: "list[Particle]",
+        particles: list[Particle],
     ) -> bool:
         for i in range(len(particles)):
             close_points_count: int = 0
@@ -53,7 +56,7 @@ class SwarmEarlyStopChecker(EarlyStopCheckerInterface):
 
     def check_velocity(
         self,
-        particles: "list[Particle]",
+        particles: list[Particle],
     ) -> bool:
         small_velocity_points_count: int = 0
     
--- src/early_stopping/stopping_params_factory.py
+++ src/early_stopping/stopping_params_factory.py
@@ -8,22 +8,3 @@ from src.early_stopping.stopping_params import STOPPING_PARAMS_REGISTER
 class StoppingParamsFactory:
     def construct(self, params_config) -> tp.Type[BaseModel]:
         return STOPPING_PARAMS_REGISTER[params_config["type"]](**params_config["params"])
-
-
-f = StoppingParamsFactory()
-
-config = {
-    'type': 'Swarm',
-    'params': {
-        'coordinate': {
-            'epsilon': 0.0001,
-            'ratio': 0.75,
-        },
-        'velocity': {
-            'epsilon': 0.0001,
-            'ratio': 0.75,
-        }
-    }
-}
-
-p = f.construct(config)
