7574e28c53f7a1271921a9f14f237fc1b96959ed
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-07-18 16:18:58 +0300

    lil style update in field factory

--- src/field/field_factory.py
+++ src/field/field_factory.py
@@ -6,8 +6,8 @@ class FieldFactory:
     def construct(self, field_config) -> Field:
         field: Field = Field(
             FieldParameters(**field_config["params"]),
-            TARGET_FUNCTION_REGISTER[field_config["type"]],
-            TARGET_FUNCTION_SYMBOLIC_REGISTER[field_config["type"]],
+            TARGET_FUNCTION_REGISTER[field_config["type"].lower()],
+            TARGET_FUNCTION_SYMBOLIC_REGISTER[field_config["type"].lower()],
         )
 
         return field
