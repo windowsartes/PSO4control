32bbf72a9172fe88e6cb98c94e87907f8d852b1e
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-10-18 12:20:47 +0300

    scheduler updated

--- src/scheduler/scheduler_factory.py
+++ src/scheduler/scheduler_factory.py
@@ -1,5 +1,3 @@
-import typing as tp
-
 from pydantic import BaseModel
 
 from src.scheduler.scheduler import SCHEDULER_REGISTER, SchedulerInteface
@@ -9,8 +7,11 @@ from src.scheduler.scheduler_params_factory import SchedulerParamsFactory
 class SchedulerFactory:
     _params_factory: SchedulerParamsFactory = SchedulerParamsFactory()
 
-    def construct(self, config) -> tp.Type[SchedulerInteface]:
-        params: tp.Type[BaseModel] = self._params_factory.construct(config)
-        scheduler: tp.Type[SchedulerInteface] = SCHEDULER_REGISTER[config["type"].lower()](params)
+    def construct(  # type: ignore
+        self,
+        config,
+    ) -> SchedulerInteface:
+        params: BaseModel = self._params_factory.construct(config)
+        scheduler: SchedulerInteface = SCHEDULER_REGISTER[config["type"].lower()](params)
 
         return scheduler
