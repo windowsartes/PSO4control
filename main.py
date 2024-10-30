16f7a488c5aca55156a2a952a84d8c01f8e5d8fd
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-10-30 23:19:20 +0300

    force update: now we stores on mean and stds, not full results

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 
 import click
+import numpy as np
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
@@ -35,9 +36,9 @@ def cli(
 
                 with open(f"./{config['solver']['params']['spawn']['type']}_{n}_{r}_{noise_scale * 100}.csv", 'w') as f: 
                     results = (Parallel(n_jobs=n_jobs)(delayed(Scene(config=config).solve)() for i in range(n_iterations)))
-                    f.write('error,path\n')
-                    for (error, path) in results:
-                        f.write(f'{error},{path}\n')
+                    results = np.array(results)
+                    f.write('error mean,error std, path mean, path std\n')
+                    f.write(f'{np.mean(results[:, 0])},{np.std(results[:, 0], ddof=1)}, {np.mean(results[:, 1])}, {np.std(results[:, 1], ddof=1)}\n')
                 print(f'{i}/{total} done')
                 i+=1
 
